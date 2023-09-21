49a49ed017ebd8c0ad30c199ca963576b48b1120
Gustavo Navarro <genr1818@gmail.com>
2023-09-20 18:58:11 -0600

    Components: MyOrders and MyOrder

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,16 +1,28 @@
 import { useContext } from "react"
+import { Link, useParams } from 'react-router-dom'
 import { ShoppingCartContext } from "../../Context"
 
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 import { OrderCard } from "../../Components/OrderCard"
 
 function MyOrder() {
   const { order } = useContext(ShoppingCartContext)
+  const { id } = useParams();
+  const pathName = window.location.pathname
+
+  let index = id;
+  if (pathName === '/my-orders/last') index = order?.length - 1
 
   return (
     <>
-      <h1>MyOrder</h1>
+      <div className='relative flex items-center justify-center mb-6 w-80'>
+        <Link to='/my-orders' className='absolute left-0'>
+          <ChevronLeftIcon className='w-6 h-6 text-black cursor-pointer' />
+        </Link>
+        <h1>My Order</h1>
+      </div>
       <div className='flex flex-col w-80'>
-        {order?.slice(-1)[0].products.map(product => (
+        {order?.[index]?.products?.map(product => (
           <OrderCard
             key={product.id}
             id={product.id}
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,6 +1,25 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ShoppingCartContext } from '../../Context'
+
+import { OrdersCard } from '../../Components/OrdersCard'
+
 function MyOrders() {
+  const { order } = useContext(ShoppingCartContext)
+
   return (
-    <div>MyOrders</div>
+    <>
+      <div className='relative flex items-center justify-center mb-4 w-80'>
+        <h1 className='text-xl font-medium'>My Orders</h1>
+      </div>
+      {order.map((order, index) => (
+        <Link key={index} to={`/my-orders/${index}`}>
+          <OrdersCard
+            totalPrice={order.totalPrice}
+            totalProducts={order.totalProducts} />
+        </Link>
+      ))}
+    </>
   )
 }
 
