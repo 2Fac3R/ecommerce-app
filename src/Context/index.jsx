df60c83ccdb59851a459121d31c120e35f432f46
Gustavo Navarro <genr1818@gmail.com>
2023-09-20 20:28:32 -0600

    Refactor: renaming, filtering and searching

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,38 +3,43 @@ import { createContext, useState, useEffect } from "react"
 export const API_URL = 'https://fakestoreapi.com'
 export const ShoppingCartContext = createContext()
 
-export function ShoppingCartProvider({ children }) {
-  // Shopping Cart - Increment quantity
-  const [count, setCount] = useState(0)
+export const ShoppingCartProvider = ({ children }) => {
 
-  // ProductDetail - Open/Close
+  // Product Detail · Open/Close
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
   const openProductDetail = () => setIsProductDetailOpen(true)
   const closeProductDetail = () => setIsProductDetailOpen(false)
 
-  // ProductDetail - Show product
-  const [productDetails, setProductDetails] = useState({})
+  // Checkout Side Menu · Open/Close
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+  const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+  const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
 
-  // Shopping Cart - Add products to cart
+  // Product Detail · Show product
+  const [productToShow, setProductToShow] = useState({})
+
+  // Shopping Cart · Add items to cart
   const [cartProducts, setCartProducts] = useState([])
 
-  // CheckOutSideMenu - Open/Close
-  const [isCheckOutSideMenuOpen, setIsCheckOutSideMenuOpen] = useState(false)
-  const openCheckOutSideMenu = () => setIsCheckOutSideMenuOpen(true)
-  const closeCheckOutSideMenu = () => setIsCheckOutSideMenuOpen(false)
-
-  // Shopping Cart - Order
+  // Shopping Cart · Order
   const [order, setOrder] = useState([])
 
-  // Get products
-  const [products, setProducts] = useState([])
+  // Get items
+  const [items, setItems] = useState(null)
+  const [filteredItems, setFilteredItems] = useState(null)
+
+  // Get items by title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+
+  // Get items by category
+  const [searchByCategory, setSearchByCategory] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`${API_URL}/products`)
         const data = await response.json()
-        setProducts(data)
+        setItems(data)
       } catch (error) {
         console.error(`Error: ${error}`);
       }
@@ -42,24 +47,60 @@ export function ShoppingCartProvider({ children }) {
     fetchData()
   }, [])
 
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  const filteredItemsByCategory = (items, searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
+
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }
+
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }
+
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if (!searchType) {
+      return items
+    }
+  }
+
+  useEffect(() => {
+    if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+  }, [items, searchByTitle, searchByCategory])
+
   return (
     <ShoppingCartContext.Provider value={{
-      count,
-      setCount,
-      isProductDetailOpen,
       openProductDetail,
       closeProductDetail,
-      productDetails,
-      setProductDetails,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow,
       cartProducts,
       setCartProducts,
-      isCheckOutSideMenuOpen,
-      openCheckOutSideMenu,
-      closeCheckOutSideMenu,
+      isCheckoutSideMenuOpen,
+      openCheckoutSideMenu,
+      closeCheckoutSideMenu,
       order,
       setOrder,
-      products,
-      setProducts
+      items,
+      setItems,
+      searchByTitle,
+      setSearchByTitle,
+      filteredItems,
+      searchByCategory,
+      setSearchByCategory
     }}>
       {children}
     </ShoppingCartContext.Provider>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,22 +4,37 @@ import { ShoppingCartContext } from "../../Context"
 import { Card } from "../../Components/Card"
 import { ProductDetail } from "../../Components/ProductDetail"
 
-function Home() {
-  const { products } = useContext(ShoppingCartContext)
+export function Home() {
+  const context = useContext(ShoppingCartContext)
+
+  const renderView = () => {
+    if (context.filteredItems?.length > 0) {
+      return (
+        context.filteredItems?.map(item => (
+          <Card key={item.id} {...item} />
+        ))
+      )
+    } else {
+      return (
+        <div>We don't have anything :(</div>
+      )
+    }
+  }
 
   return (
     <div className="Home">
-      <div className="grid w-full max-w-screen-lg grid-cols-4 gap-6 mr-2">
-        {products.map((product) => (
-          <Card
-            key={product.id}
-            {...product}
-          />
-        ))}
+      <div className='relative flex items-center justify-center mb-4 w-80'>
+        <h1 className='text-xl font-medium'>Exclusive Products</h1>
+      </div>
+      <input
+        type="text"
+        placeholder='Search a product'
+        className='p-4 mb-4 border border-black rounded-lg w-80 focus:outline-none'
+        onChange={(event) => context.setSearchByTitle(event.target.value)} />
+      <div className='grid w-full max-w-screen-lg grid-cols-4 gap-4'>
+        {renderView()}
       </div>
       <ProductDetail />
     </div>
   )
 }
-
-export { Home }
